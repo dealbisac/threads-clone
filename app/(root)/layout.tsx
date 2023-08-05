4cc07e5a6ab17c64f948d36bdef77d63f8f64724
dealbisac <chand.dipendra19@gmail.com>
2023-08-05 16:29:43 +0200

    Feature: complete the topbar

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,47 @@
+import '../globals.css'
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import Topbar from '@/components/shared/Topbar';
+import LeftSidebar from '@/components/shared/LeftSidebar';
+import RightSidebar from '@/components/shared/RightSidebar';
+import Bottombar from '@/components/shared/Bottombar';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Threads Clone',
+  description: 'A Next.js Clone of Meta Threads',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={inter.className}>
+          <Topbar />
+
+          <main>
+            <LeftSidebar />
+
+            <section className='main-container'>
+              <div className="w-full max-w-4xl">
+                {children}
+              </div>
+            </section>
+
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
+
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+//app/page.tsx
+export default function Home() {
+  return (
+    <>
+      <h1 className="head-text text-left">Home</h1>
+    </>
+  )
+}
