3a8e14173dd81370ae802a4dc009bd6cbc6c36da
dealbisac <chand.dipendra19@gmail.com>
2023-08-06 12:41:05 +0200

    Feature: complete form design

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -100,11 +100,11 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                     control={form.control}
                     name="name"
                     render={({ field }) => (
-                        <FormItem className="flex items-center gap-3 w-full">
+                        <FormItem className="flex flex-col gap-3 w-full">
                             <FormLabel className="text-base-semibold text-light-2">
                                 Name
                             </FormLabel>
-                            <FormControl className="flex-1 text-base-semibold text-gray-200">
+                            <FormControl>
                                 <Input
                                     type="text"
                                     className="account-form_input no-focus"
@@ -119,11 +119,11 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                     control={form.control}
                     name="username"
                     render={({ field }) => (
-                        <FormItem className="flex items-center gap-3 w-full">
+                        <FormItem className="flex flex-col gap-3 w-full">
                             <FormLabel className="text-base-semibold text-light-2">
                                 Username
                             </FormLabel>
-                            <FormControl className="flex-1 text-base-semibold text-gray-200">
+                            <FormControl>
                                 <Input
                                     type="text"
                                     className="account-form_input no-focus"
@@ -138,11 +138,11 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                     control={form.control}
                     name="bio"
                     render={({ field }) => (
-                        <FormItem className="flex items-center gap-3 w-full">
+                        <FormItem className="flex flex-col gap-3 w-full">
                             <FormLabel className="text-base-semibold text-light-2">
                                 Bio
                             </FormLabel>
-                            <FormControl className="flex-1 text-base-semibold text-gray-200">
+                            <FormControl>
                                 <Textarea
                                     rows={10}
                                     className="account-form_input no-focus"
@@ -152,7 +152,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                         </FormItem>
                     )}
                 />
-                <Button type="submit">Submit</Button>
+                <Button type="submit" className="bg-primary-500">Submit</Button>
             </form>
         </Form>
     )
