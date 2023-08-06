50aa3bf2b8ea3e8cc97429457f4736b70417e7c6
dealbisac <chand.dipendra19@gmail.com>
2023-08-06 13:21:45 +0200

    Feature: complete frontend form and submit

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -17,7 +17,9 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { userValidation } from "@/lib/validations/user";
 import * as z from "zod";
 import Image from "next/image";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
+import { isBase64Image } from "@/lib/utils";
+import { useUploadThing } from "@/lib/uploadthing";
 
 interface Props {
     user: {
@@ -32,24 +34,58 @@ interface Props {
 }
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
+    const [files, setFiles] = useState<File[]>([]);
+    const { startUpload } = useUploadThing("media");
+
     const form = useForm({
         resolver: zodResolver(userValidation),
         defaultValues: {
-            profile_photo: '',
-            name: '',
-            username: '',
-            bio: '',
+            profile_photo: user?.image || '',
+            name: user?.name || '',
+            username: user?.username || '',
+            bio: user?.bio || '',
         }
     });
 
-    const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
+    const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
         e.preventDefault();
+
+        const fileReader = new FileReader();
+
+        if (e.target.files && e.target.files.length > 0) {
+            const file = e.target.files[0];
+
+            setFiles(Array.from(e.target.files));
+
+            if (!file.type.includes('image')) return;
+
+            fileReader.onload = async (event) => {
+                const imageDataUrl = event.target?.result?.toString() || '';
+
+                fieldChange(imageDataUrl);
+            }
+
+            fileReader.readAsDataURL(file);
+        }
+
     }
 
-    function onSubmit(values: z.infer<typeof userValidation>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+    const onSubmit = async (values: z.infer<typeof userValidation>) => {
+        // It will validate your inputs before invoking `onSubmit` ✅
+        const blob = values.profile_photo;
+
+        const hasImageChanged = isBase64Image(blob);
+
+        if (hasImageChanged) {
+            const imgRes = await startUpload(files);
+
+            if (imgRes && imgRes[0].fileUrl) {
+                values.profile_photo = imgRes[0].fileUrl;
+            }
+        }
+
+        //TODO: Update user profile
+        
     }
 
     return (
--- next.config.js
+++ next.config.js
@@ -1,4 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        serverActions: true,
+        serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+        remotePatterns: [
+            // Match any image URL hosted on{website}
+            // and optimizes it at build time.
+            // https://nextjs.org/docs/basic-features/image-optimization#domains
+            {
+                protocol: "https",
+                hostname: "img.clerk.com",
+            },
+            {
+                protocol: "https",
+                hostname: "images.clerk.dev",
+            },
+            {
+                protocol: "https",
+                hostname: "uploadthing.com",
+            },
+            {
+                protocol: "https",
+                hostname: "placehold.co",
+            },
+        ],
+        typescript: {
+            ignoreBuildErrors: true,
+        },
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
