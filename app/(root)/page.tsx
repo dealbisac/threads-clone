a2135f32778bc272a4d10e86f4a09a9b5a5e3dcf
dealbisac <chand.dipendra19@gmail.com>
2023-08-20 13:04:16 +0200

    Feature: complete thread card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchPosts } from "@/lib/actions/thread.actions";
 import { currentUser } from "@clerk/nextjs";
 
@@ -23,7 +24,7 @@ export default async function Home() {
                   <ThreadCard
                     key={post._id}
                     id={post._id}
-                    currentUserId={user?.id}
+                    currentUserId={user?.id || ""}
                     parentId={post.parentId}
                     content={post.text}
                     author={post.author}
