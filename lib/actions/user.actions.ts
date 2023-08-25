c35a8c02ee879b2109515a3b77a747db406cdb60
dealbisac <chand.dipendra19@gmail.com>
2023-08-25 10:20:11 +0200

    Feature: partial complete of search page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDatabase } from "../mongoose"
 import Thread from "../models/thread.model";
+import { FilterQuery, SortOrder } from "mongoose";
 
 interface Params {
     userId: String,
@@ -90,3 +91,56 @@ export async function fetchUserPosts(userId: String) {
         throw new Error(`Failed to fetch user posts: ${error.message}`);
     }
 }
+
+// Fetch users
+export async function fetchUsers({
+    userId,
+    searchString,
+    pageNumber = 1,
+    pageSize = 20,
+    sortBy = "desc",
+}: {
+    userId: String,
+    searchString?: String,
+    pageNumber?: Number,
+    pageSize?: Number,
+    sortBy?: SortOrder
+}) {
+    connectToDatabase();
+
+    try {
+        const skipAmount = (pageNumber - 1) * pageSize;
+
+        const regex = new RegExp(searchString, "i");
+
+        const query: FilterQuery<typeof User> = {
+            id: { $ne: userId },
+        }
+
+        if (searchString.trim() !== "") {
+            query.$or = [
+                { username: { $regex: regex } },
+                { name: { $regex: regex } }
+            ]
+        }
+
+        const sortOptions = { createdAt: sortBy };
+
+        const usersQuery = User.find(query)
+            .sort(sortOptions)
+            .skip(skipAmount)
+            .limit(pageSize);
+
+        const totalUsersCount = await User.countDocuments(query);
+
+        const users = await usersQuery.exec();
+
+        const isNext = totalUsersCount > skipAmount + users.length;
+
+        return { users, isNext };
+
+    } catch (error: any) {
+        throw new Error(`Failed to fetch users: ${error.message}`);
+    }
+}
+
